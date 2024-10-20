881a24d3186ed7abca1920aaee492453dc35f9d9
Nicolás <nicolas.lopezfernandez1@usp.ceu.es>
2024-10-20 20:39:42 +0200

    1.5.8 y 1.5.9

---
+++ dominio/Provincia.java
@@ -0,0 +1,18 @@
+import .java.util.ArrayList
+  
+public class Provincia {
+  private List<Municipio> municipios;
+
+  public Provincia (String) {
+  this.provincia = new ArrayList<>();
+}
+  public void agregarMunicipios(Municipio municipio) {
+    municipios.add(municipio);
+  }
+  public int contarHabitantes() {
+    int totalDeHabitantes = 0
+    for (Municipio municipio : municipios);
+      totalDeHabitantes += municipio.contarHabitantes();
+  }
+  return totalDehabitantes;
+  }}  
